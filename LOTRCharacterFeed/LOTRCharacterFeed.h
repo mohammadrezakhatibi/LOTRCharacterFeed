1b218a4dfa5613cf0e9a85410abdc3592ba18938
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-12-21 14:03:28 +0200

    Initial Commit

---
+++ LOTRCharacterFeed/LOTRCharacterFeed.h
@@ -0,0 +1,18 @@
+//
+//  LOTRCharacterFeed.h
+//  LOTRCharacterFeed
+//
+//  Created by Mohammadreza on 12/21/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LOTRCharacterFeed.
+FOUNDATION_EXPORT double LOTRCharacterFeedVersionNumber;
+
+//! Project version string for LOTRCharacterFeed.
+FOUNDATION_EXPORT const unsigned char LOTRCharacterFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LOTRCharacterFeed/PublicHeader.h>
+
+
