dd18ec4b627ab4861ac72b1689720c31d2c05eb3
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-12-21 15:37:05 +0200

    Make sure `CharacterLoader.load` twice requests data from url twice

--- LOTRCharacterFeedTests/RemoteCharacterLoaderTests.swift
+++ LOTRCharacterFeedTests/RemoteCharacterLoaderTests.swift
@@ -43,6 +43,16 @@ final class RemoteCharacterLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = anyURL()
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteCharacterLoader, client: HTTPClientSpy) {
