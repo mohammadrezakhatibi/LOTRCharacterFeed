5a46e0a6a4adb1f7b750958e4d84ee2bba561551
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-12-27 22:42:53 +0200

    `URLSessionHTTPClient.get` from url requests data from url

--- LOTRCharacterFeedTests/URLSessionHTTPClientTests.swift
+++ LOTRCharacterFeedTests/URLSessionHTTPClientTests.swift
@@ -13,6 +13,10 @@ final class URLSessionHTTPClient {
     init(session: URLSession) {
         self.session = session
     }
+    
+    func get(url: URL) {
+        session.dataTask(with: url, completionHandler: { _,_,_ in })
+    }
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
@@ -23,13 +27,23 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         XCTAssertTrue(session.requestedURLs.isEmpty)
     }
+    
+    func test_getFromURL_requestsDataFromURL() {
+        let url = anyURL()
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+            
+        sut.get(url: url)
+        
+        XCTAssertEqual(session.requestedURLs, [url])
+    }
 
     
     private class URLSessionSpy: URLSession {
         var requestedURLs = [URL]()
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            
+            requestedURLs.append(url)
             return FakeURLSessionTask()
         }
     }
