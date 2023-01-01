1e7b66bb1a79c9b03452b94742d5b17b31f2ed63
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-01-01 23:38:19 +0200

    LoadCharacters displays character name and race on successful load

--- LOTRCharacterFeediOSTests/LOTRCharacterFeediOSTests.swift
+++ LOTRCharacterFeediOSTests/LOTRCharacterFeediOSTests.swift
@@ -47,9 +47,13 @@ struct CharacterFeed: View {
 }
 
 struct CharacterRow: View {
-    var character: CharacterItem
+    private(set) var character: CharacterItem
+    
     public var body: some View {
-        return Text(character.name)
+        return VStack {
+            Text(character.name)
+            Text(character.race).id("character.race")
+        }
     }
 }
 
@@ -65,8 +69,25 @@ final class LOTRCharacterFeediOSTests: XCTestCase {
         var sut = makeSUT(result: .success(items))
         
         let exp = sut.on(\.didAppear) { view in
-            let count = view.findAll(CharacterRow.self).count
-            XCTAssertEqual(count, items.count)
+            let cells = view.findAll(CharacterRow.self)
+            XCTAssertEqual(cells.count, items.count)
+        }
+        
+        ViewHosting.host(view: sut)
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_loadCharacter_displaysCharacterNameAndRaceOnSuccessfulLoadCharacter() throws {
+        
+        let items = makeItems()
+        var sut = makeSUT(result: .success(items))
+        
+        let exp = sut.on(\.didAppear) { view in
+            let row = view.findAll(CharacterRow.self)
+            try items.enumerated().forEach { index, item in
+                XCTAssertEqual(try row[index].vStack().first?.text().string(), item.name)
+                XCTAssertEqual(try row[index].vStack().last?.text().string(), item.race)
+            }
         }
         
         ViewHosting.host(view: sut)
@@ -84,8 +105,8 @@ final class LOTRCharacterFeediOSTests: XCTestCase {
     
     private func makeItems() -> [CharacterItem] {
         return [
-            CharacterItem(id: "id", height: "", race: "", gender: "", birth: "", spouse: "", death: "", realm: "", hair: "", name: "Frodo", wikiURL: nil),
-            CharacterItem(id: "id", height: "", race: "", gender: "", birth: "", spouse: "", death: "", realm: "", hair: "", name: "Aragorn", wikiURL: nil)
+            CharacterItem(id: "id", height: "", race: "human", gender: "", birth: "", spouse: "", death: "", realm: "", hair: "", name: "Frodo", wikiURL: nil),
+            CharacterItem(id: "id", height: "", race: "elf", gender: "", birth: "", spouse: "", death: "", realm: "", hair: "", name: "Aragorn", wikiURL: nil)
         ]
     }
     
