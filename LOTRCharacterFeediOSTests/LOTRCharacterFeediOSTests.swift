547c123afedd7e515f0dddb84cc5c3b2b82a2b03
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-01-01 21:44:08 +0200

    `LoadCharacters` delivers a list of characters

--- LOTRCharacterFeediOSTests/LOTRCharacterFeediOSTests.swift
+++ LOTRCharacterFeediOSTests/LOTRCharacterFeediOSTests.swift
@@ -12,9 +12,11 @@ import LOTRCharacterFeed
 import LOTRCharacterFeediOS
 
 struct CharacterFeed: View {
-    let loader: CharacterLoader
+    private let loader: CharacterLoader
+    public var didAppear: ((Self) -> Void)?
+    @State var characters: [CharacterItem] = []
     
-    let columns = [
+    private let columns = [
         GridItem(.flexible(), spacing: 16),
         GridItem(.flexible(), spacing: 16),
     ]
@@ -26,21 +28,67 @@ struct CharacterFeed: View {
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns) {
-                
+                ForEach(characters, id: \.id) { character in
+                    CharacterRow(character: character)
+                }
             }
         }
+        .onAppear {
+            loadCharacters()
+            didAppear?(self)
+        }
+    }
+    
+    private func loadCharacters() {
+        loader.load { result in
+            characters = try! result.get()
+        }
+    }
+}
+
+struct CharacterRow: View {
+    var character: CharacterItem
+    public var body: some View {
+        return Text(character.name)
     }
 }
 
 final class LOTRCharacterFeediOSTests: XCTestCase {
     
     func test_init_createsAList() throws {
-        let loader = CharacterLoaderStub(result: .success([]))
-        let sut = CharacterFeed(loader: loader)
+        let sut = makeSUT()
         XCTAssertNoThrow(try sut.inspect().scrollView().lazyVGrid())
+    }
+
+    func test_loadCharacter_deliversAListOfCharacters() throws {
+        let items = makeItems()
+        var sut = makeSUT(result: .success(items))
+        
+        let exp = sut.on(\.didAppear) { view in
+            let count = view.findAll(CharacterRow.self).count
+            XCTAssertEqual(count, items.count)
+        }
+        
+        ViewHosting.host(view: sut)
+        wait(for: [exp], timeout: 1.0)
     }
     
     // MARK: - Helper
+    
+    private func makeSUT(result: CharacterLoader.Result = .success([])) -> CharacterFeed {
+        let loader = CharacterLoaderStub(result: result)
+        let sut = CharacterFeed(loader: loader)
+        
+        return sut
+    }
+    
+    private func makeItems() -> [CharacterItem] {
+        return [
+            CharacterItem(id: "id", height: "", race: "", gender: "", birth: "", spouse: "", death: "", realm: "", hair: "", name: "Frodo", wikiURL: nil),
+            CharacterItem(id: "id", height: "", race: "", gender: "", birth: "", spouse: "", death: "", realm: "", hair: "", name: "Aragorn", wikiURL: nil)
+        ]
+    }
+    
     private final class CharacterLoaderStub: CharacterLoader {
                 
         private let result: CharacterLoader.Result
