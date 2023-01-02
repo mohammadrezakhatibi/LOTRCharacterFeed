acc6fd86cd60ed1c819184c631613d1a647bc8a2
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-01-02 22:27:08 +0200

    Move production codes to their own files

--- LOTRCharacterFeediOS/CharacterFeed.swift
+++ LOTRCharacterFeediOS/CharacterFeed.swift
@@ -6,15 +6,45 @@
 //
 
 import SwiftUI
+import LOTRCharacterFeed
 
 struct CharacterFeed: View {
+    private let loader: CharacterLoader
+    public var didAppear: ((Self) -> Void)?
+    @State var characters: [CharacterItem] = []
+    
+    private let columns = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16),
+    ]
+    
+    init(loader: CharacterLoader) {
+        self.loader = loader
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(characters, id: \.id) { character in
+                    CharacterRow(character: character)
+                }
+            }
+        }
+        .onAppear {
+            loadCharacters()
+            didAppear?(self)
+        }
+    }
+    
+    private func loadCharacters() {
+        loader.load { result in
+            characters = try! result.get()
+        }
     }
 }
 
 struct CharacterFeed_Previews: PreviewProvider {
     static var previews: some View {
-        CharacterFeed()
+        EmptyView()
     }
 }
