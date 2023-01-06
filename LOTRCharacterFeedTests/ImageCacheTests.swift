10810bfa1e396298d078ce7538a460ea1a8a9870
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-01-06 17:19:44 +0200

    LoadImageData delivers error on loader failure

--- LOTRCharacterFeedTests/ImageCacheTests.swift
+++ LOTRCharacterFeedTests/ImageCacheTests.swift
@@ -18,8 +18,10 @@ final class ImageCache {
         self.cache = cache
     }
     
-    func loadImageData(url: URL) {
-        _ = loader.loadImageData(url: url) { _ in }
+    typealias Result = Swift.Result<(Data), Error>
+    
+    func loadImageData(url: URL, completion: @escaping (Result) -> Void) {
+        _ = loader.loadImageData(url: url, completion: completion)
     }
     
 }
@@ -41,17 +43,48 @@ final class ImageCacheTests: XCTestCase {
         let cache = URLCacheSpy(memoryCapacity: 10_000_000, diskCapacity: 100_000_000)
         let sut = ImageCache(loader: loader, cache: cache)
         
-        sut.loadImageData(url: anyURL())
+        sut.loadImageData(url: url) { _ in }
         
         XCTAssertEqual(loader.receivedURLs, [url])
         XCTAssertEqual(cache.numberOfCalls, 0)
+    }
+    
+    func test_loadImageData_deliversErrorOnLoaderFailure() {
+        let url = anyURL()
+        let error = anyNSError()
+        let loader = CharacterImageDataLoaderSpy()
+        let cache = URLCacheSpy(memoryCapacity: 10_000_000, diskCapacity: 100_000_000)
+        let sut = ImageCache(loader: loader, cache: cache)
+        
+        let exp = expectation(description: "Wait for load completion")
+        var receivedError: Error?
+        sut.loadImageData(url: url) { result in
+            switch result {
+                case let .failure(error):
+                    receivedError = error
+                default:
+                    XCTFail("Expected failure, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        loader.complete(with: error)
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(loader.receivedURLs, [url])
+        XCTAssertEqual(cache.numberOfCalls, 0)
+        XCTAssertEqual(receivedError as NSError?, error)
     }
     
     // MARK: - Helper
     
     private final class CharacterImageDataLoaderSpy: CharacterImageDataLoader {
         
-        var receivedURLs = [URL]()
+        var receivedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        private var messages = [(url: URL, completion: (CharacterImageDataLoader.Result) -> Void)]()
         
         private class Task: CharacterImageDataLoaderTask {
             func cancel() {
@@ -60,8 +93,12 @@ final class ImageCacheTests: XCTestCase {
         }
         
         func loadImageData(url: URL, completion: @escaping (CharacterImageDataLoader.Result) -> Void) -> CharacterImageDataLoaderTask {
-            receivedURLs.append(url)
+            messages.append((url, completion))
             return Task()
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
         }
     }
     
