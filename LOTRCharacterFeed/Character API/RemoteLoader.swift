7b57cb34c1f360f73a43c117989864a30c84000e
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-02-21 15:55:23 +0200

    Implement generic remote loader

--- LOTRCharacterFeed/Character API/RemoteLoader.swift
+++ LOTRCharacterFeed/Character API/RemoteLoader.swift
@@ -1,61 +1,43 @@
 import Foundation
 
-public final class RemoteLoader: CharacterLoader {
+public final class RemoteLoader<Resource> {
     
     let request: URLRequest
     let client: HTTPClient
+    let mapper: Mapper
     
+    public typealias Mapper = (Data, HTTPURLResponse) throws -> Resource
+    public typealias Result = Swift.Result<Resource, RemoteLoader.Error>
     public enum Error: Swift.Error {
         case invalidData
         case connectivity
         case unauthorized
     }
     
-    public init(request: URLRequest, client: HTTPClient) {
+    public init(request: URLRequest, client: HTTPClient, mapper: @escaping Mapper) {
         self.request = request
         self.client = client
+        self.mapper = mapper
     }
     
-    public func load(completion: @escaping (CharacterLoader.Result) -> Void) {
+    public func load(completion: @escaping (Result) -> Void) {
         client.get(from: request) { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
+                case let .success((data, response)):
+                    completion(self.map(data, with: response))
                 case .failure:
                     completion(.failure(RemoteLoader.Error.connectivity))
-                case let .success((data, response)):
-                    guard let self = self else { return }
-                    completion(self.map(data, with: response))
             }
         }
     }
     
-    private func map(_ data: Data, with response: HTTPURLResponse) -> CharacterLoader.Result {
+    private func map(_ data: Data, with response: HTTPURLResponse) -> Result {
         do {
-            let items = try ItemMapper.map(data, response: response)
-            return .success(items.toModel())
-        } catch(let error) {
-            return .failure(error)
+            return .success(try mapper(data, response))
+        } catch {
+            return .failure(Error.invalidData)
         }
-    }
-}
-
-public struct ItemMapper {
-    
-    private struct Root: Codable {
-        var docs: [RemoteCharacterItem]
-        var total: Int
-        var limit: Int
-        var offset : Int
-        var page: Int
-        var pages: Int
-        
-    }
-    
-    public static func map(_ data: Data, response: HTTPURLResponse) throws -> [RemoteCharacterItem] {
-        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            throw response.isUnauthorized
-            ? RemoteLoader.Error.unauthorized
-            : RemoteLoader.Error.invalidData
-        }
-        return root.docs
     }
 }
--- LOTRCharacterFeedTests/RemoteLoaderTests.swift
+++ LOTRCharacterFeedTests/RemoteLoaderTests.swift
@@ -33,91 +33,30 @@ final class RemoteLoaderTests: XCTestCase {
         let (sut, client) = makeSUT(url: url)
         let anError = anyNSError()
         
-        expect(sut, toCompleteWith: .failure(RemoteLoader.Error.connectivity), when: {
+        expect(sut, toCompleteWith: .failure(RemoteLoader<String>.Error.connectivity), when: {
             client.complete(with: anError)
         })
     }
     
-    func test_load_deliversErrorOnNon200HTTPClientResponse() {
+    func test_load_deliversErrorOnMapperError() {
         let url = anyURL()
-        let (sut, client) = makeSUT(url: url)
-        let data = anyData()
-        let samples = [100, 199, 300, 399, 400, 500]
-        
-        samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: failure(.invalidData), when: {
-                client.complete(withStatusCode: code, data: data, at: index)
-            })
-        }
-    }
-    
-    func test_load_deliversUnAuthorizedErrorOn400HTTPClientResponse() {
-        let url = anyURL()
-        let (sut, client) = makeSUT(url: url)
-        let data = anyData()
-        
-        expect(sut, toCompleteWith: failure(.unauthorized), when: {
-            client.complete(withStatusCode: 401, data: data)
+        let (sut, client) = makeSUT(url: url, mapper: { _,_ in
+            throw anyNSError()
         })
-    }
-    
-    func test_load_deliversErrorOn200HTTPClientResponseWithInvalidJSON() {
-        let url = anyURL()
-        let (sut, client) = makeSUT(url: url)
         let invalidData = anyData()
-        
         expect(sut, toCompleteWith: failure(.invalidData), when: {
             client.complete(withStatusCode: 200, data: invalidData, at: 0)
         })
     }
     
-    func test_load_deliversEmptyItemOn200HTTPClientResponseWithEmptyJSON() {
-        let (sut, client) = makeSUT()
-        
-        expect(sut, toCompleteWith: .success([]), when: {
-            let emptyListJSON = makeItemJSON([])
-            client.complete(withStatusCode: 200, data: emptyListJSON)
+    func test_load_deliversMappedResource() {
+        let resource = "a resource"
+        let (sut, client) = makeSUT(mapper: { data,_ in
+            String(data: data, encoding: .utf8)!
         })
-    }
-    
-    func test_load_deliversItemsOn200HTTPClientResponseWithValidJSON() {
-        let (sut, client) = makeSUT()
         
-        let item1 = makeItem(
-            id: "5cd99d4bde30eff6ebccfbe6",
-            height: "198cm (6'6\")",
-            race: "Human",
-            gender: "Female",
-            birth: "March 1 ,2931",
-            spouse: "Arwen",
-            death: "FO 120",
-            realm: "Reunited Kingdom, Arnor, Gondor",
-            hair: "Dark",
-            name: "Aragorn II Elessar",
-            wikiURL: URL(string: "http://lotr.wikia.com//wiki/Aragorn_II_Elessar")!,
-            imageURL: URL(string: "http://any-image-url.com")!
-        )
-        
-        let item2 = makeItem(
-            id: "5cd99d4bde3ewef6ebccfbe6",
-            height: "208cm (6'6\")",
-            race: "Elves",
-            gender: "Male",
-            birth: "March 1 ,931",
-            spouse: "Narbia",
-            death: "Departed to Aman in FO 120 from Ithilien",
-            realm: "Reunited Kingdom, Arnor, Gondor",
-            hair: "Golden",
-            name: "Legolas",
-            wikiURL: URL(string: "http://lotr.wikia.com//wiki/Aragorn_II_Elessar")!,
-            imageURL: URL(string: "http://any-image-url.com")!
-        )
-        
-        let items = [item1.model, item2.model]
-    
-        expect(sut, toCompleteWith: .success(items), when: {
-            let json = makeItemJSON([item1.json, item2.json])
-            client.complete(withStatusCode: 200, data: json)
+        expect(sut, toCompleteWith: .success(resource), when: {
+            client.complete(withStatusCode: 200, data: Data(resource.utf8))
         })
     }
     
@@ -125,10 +64,10 @@ final class RemoteLoaderTests: XCTestCase {
         let url = anyURL()
         let client = HTTPClientSpy()
         let request = MockRequest(url: url).create()
-        var sut: RemoteLoader? = RemoteLoader(request: request, client: client)
+        var sut: RemoteLoader<String>? = RemoteLoader<String>(request: request, client: client, mapper: { _,_ in "" })
         
         
-        var capturedResult: [CharacterLoader.Result] = []
+        var capturedResult: [ RemoteLoader<String>.Result] = []
         sut?.load(completion: { capturedResult.append($0) })
         
         sut = nil
@@ -137,38 +76,24 @@ final class RemoteLoaderTests: XCTestCase {
         XCTAssertTrue(capturedResult.isEmpty)
     }
     
-    func test_toModel_deliverConvertedCharacterItem() {
-        let model1 = RemoteCharacterItem(id: UUID().uuidString, height: "a height", race: "a race", gender: "a gender", birth: "a birth", spouse: "a spouse", death: "a death", realm: "a realm", hair: "a hair", name: "a name", wikiUrl: URL(string: "https://any-url.com")!, imageUrl: URL(string: "https://any-image-url.com")!)
-        let model2 = RemoteCharacterItem(id: UUID().uuidString, height: "a height", race: "a race", gender: "a gender", birth: "a birth", spouse: "a spouse", death: "a death", realm: "a realm", hair: "a hair", name: "a name", wikiUrl: URL(string: "https://any-url.com")!, imageUrl: URL(string: "https://any-image-url.com")!)
-        
-        let items = [model1, model2]
-        
-        XCTAssertEqual(model1._id, items.toModel().first?.id)
-        XCTAssertEqual(model1.height, items.toModel().first?.height)
-        XCTAssertEqual(model1.race, items.toModel().first?.race)
-        XCTAssertEqual(model1.gender, items.toModel().first?.gender)
-        XCTAssertEqual(model1.birth, items.toModel().first?.birth)
-        XCTAssertEqual(model1.spouse, items.toModel().first?.spouse)
-        XCTAssertEqual(model1.death, items.toModel().first?.death)
-        XCTAssertEqual(model1.realm, items.toModel().first?.realm)
-        XCTAssertEqual(model1.hair, items.toModel().first?.hair)
-        XCTAssertEqual(model1.name, items.toModel().first?.name)
-        XCTAssertEqual(model2._id, items.toModel()[1].id)
-    }
-    
     // MARK: - Helpers
     
-    private func makeSUT(url: URL = URL(string: "http://any-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteLoader, client: HTTPClientSpy) {
+    private func makeSUT(
+        url: URL = URL(string: "http://any-url.com")!,
+        mapper: @escaping RemoteLoader<String>.Mapper = { _,_ in "" },
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: RemoteLoader<String>, client: HTTPClientSpy) {
         
         let client = HTTPClientSpy()
         let request = MockRequest(url: url).create()
-        let sut = RemoteLoader(request: request, client: client)
+        let sut = RemoteLoader<String>(request: request, client: client, mapper: mapper)
         trackingForMemoryLeaks(client, file: file, line: line)
         trackingForMemoryLeaks(sut, file: file, line: line)
         return (sut, client)
     }
     
-    func expect(_ sut: RemoteLoader, toCompleteWith expectedResult: CharacterLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+    func expect(_ sut: RemoteLoader<String>, toCompleteWith expectedResult: RemoteLoader<String>.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
     
         let exp = expectation(description: "Wait for load completion")
         sut.load { receivedResult in
@@ -176,7 +101,7 @@ final class RemoteLoaderTests: XCTestCase {
                 case let (.success(expectedItems), .success(receivedItems)):
                     XCTAssertEqual(expectedItems, receivedItems, file: file, line: line)
                     
-                case let (.failure(expectedError as RemoteLoader.Error), .failure(receivedError as RemoteLoader.Error)):
+                case let (.failure(expectedError), .failure(receivedError)):
                     XCTAssertEqual(expectedError, receivedError, file: file, line: line)
                 default:
                     XCTFail("Expected to get \(expectedResult), got \(receivedResult) instead", file: file, line: line)
@@ -243,7 +168,7 @@ final class RemoteLoaderTests: XCTestCase {
         return try! JSONSerialization.data(withJSONObject: json)
     }
     
-    private func failure(_ error: RemoteLoader.Error) -> CharacterLoader.Result {
+    private func failure(_ error: RemoteLoader<String>.Error) -> RemoteLoader<String>.Result {
         return .failure(error)
     }
 
